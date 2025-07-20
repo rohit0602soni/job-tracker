d5eb77f2ed0d9eb21c1faa1bb1bae0e26c0d6b9b
rohit0602soni <rohit0602soni@gmail.com>
2025-07-20 21:48:41 +0530

    prisma setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main className="flex justify-center items-center h-screen">
-      <h1 className="text-3xl font-bold">Job Tracker App</h1>
+      <Button>Click me</Button>
     </main>
   );
 }
-
